a7c8c5bfe63fbbad4bd46fb7aec05d5b8acc08db
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-02-21 15:13:08 +0100

    -Added actual program

---
+++ Pointersv1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Pointersv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int x = 100;
+                int* xptr = &x;
+                WriteLine((int)xptr);           //displays pointer value
+                WriteLine(*xptr);               //displays pointed value
+            }
+            ReadKey();
+        }
+    }
+}
